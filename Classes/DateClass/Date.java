85865458090aa41e5ddec80e54011c4da98e850f
Safal Adhikari <safal.adhikari1@outlook.com>
2020-07-04 09:54:36 -0700

    added new methods on Date Class

--- Classes/DateClass/Date.java
+++ Classes/DateClass/Date.java
@@ -3,17 +3,17 @@
 public class Date
 {
    // fields
-   int year;
-   int month;
-   int day;
+   private int year;
+   private int month;
+   private int day;
    
    // constructors - initialize the state of created objects
    
    public Date(int year, int month, int day)
    {
-      this.year = year;
-      this.month = month;
-      this.day = day;
+      setYear(year);
+      setMonth(month);
+      setDay(day);
    }
    
    public Date(Date other)
@@ -22,6 +22,46 @@ public class Date
    }
    
    // methods
+   
+   public int getDay()
+   {
+      return this.day;
+   }
+   
+   public int getMonth()
+   {
+      return this.month;
+   }
+   
+   public int getYear()
+   {
+      return this.year;
+   }
+   
+   public void setDay(int day)
+   {
+      if(day < 1 || day > getDaysInMonth())
+      {
+         throw new IllegalArgumentException("Illegal day: " + day);
+      }
+      
+      this.day = day;
+   }
+   
+   public void setMonth(int month)
+   {
+      if(month < 1 || month > 12)
+      {
+         throw new IllegalArgumentException("Illegal month: " + month);
+      }
+      
+      this.month = month;
+   }
+   
+   public void setYear(int year)
+   {
+      this.year = year;
+   }
    
    // Returns the number of days in the month
    public int getDaysInMonth()
@@ -62,8 +102,66 @@ public class Date
    
    // Leap Years : (those divisible by 4 but not by 100), (divisible by 400)          
    // have 366 days (extra day on Feb, 29); other years have 365.
+   
+   // Returns true if this Date occurs in a leap year.
    public boolean isLeapYear()
    {
       return (this.year % 400 == 0 || (this.year % 4 == 0 && this.year % 100 != 0));
    }
+   
+   // Advances a Date
+   public void nextDay()
+   {
+      this.day++;
+      
+      if(this.day > getDaysInMonth())
+      {
+         this.month++;
+         this.day = 1;
+         
+         if(this.month > 12)
+         {
+            this.year = 1;
+         }
+      }     
+   }
+   
+   public int daysBetween(Date other)
+   {
+      // count number of days
+      // start at this date, move forward until we reach 'other'
+      
+      int count = 0;
+          
+      // assumes that this date < other date
+      if(before(other))
+      {
+         Date temp = new Date(this);
+         
+         while(temp.year != other.year || temp.month != other.month || temp.day != other.day)
+         {
+            temp.nextDay();
+            count++;
+         }   
+      }
+      else
+      {
+         // this date is >= other date        
+         Date temp = new Date(other);
+         
+         while(temp.year != this.year || temp.month != this.month || temp.day != this.day)
+         {
+            temp.nextDay();
+            count++;
+         }         
+      }    
+      return count;
+   }
+   
+   public boolean before(Date other)
+   {
+      return (this.year < other.year || 
+      this.year == other.year && this.month < other.month || 
+      this.month == other.month && this.day < other.day);
+   } 
 }
--- Classes/DateClass/DateMain.java
+++ Classes/DateClass/DateMain.java
@@ -8,17 +8,18 @@ public class DateMain
       Date d2 = new Date(2008, 2, 3);
       Date d3 = new Date(d1);
       
+      Date d4 = new Date(-2345, 13, -8);
+      
+      System.out.println(d1.getYear());
       System.out.println(d1.getDaysInMonth());
-      System.out.println(d1.isLeapYear());
       System.out.println(d1.getDaysInYear());
+      System.out.println(d1.isLeapYear());
       
+      System.out.println(d2.getYear());
       System.out.println(d2.getDaysInMonth());
-      System.out.println(d2.isLeapYear());
       System.out.println(d2.getDaysInYear());
-      
-      System.out.println(d3.getDaysInMonth());
-      System.out.println(d3.isLeapYear());
-      System.out.println(d3.getDaysInYear());
+      System.out.println(d2.isLeapYear());
+   
          
    }
 }  
