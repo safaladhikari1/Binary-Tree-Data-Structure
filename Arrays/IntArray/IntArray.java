86284f32ecfaaf2a79e42fc367da1ac0e93f8ac9
Safal Adhikari <safal.adhikari1@outlook.com>
2020-07-09 23:40:26 -0700

    made changes to IntArray class

--- Arrays/IntArray/IntArray.java
+++ Arrays/IntArray/IntArray.java
@@ -1,46 +1,33 @@
 public class IntArray
-{
-   // field
-   private int[] intTestArray;
-   
-   // constructor
-   public IntArray(int[] list)
-   {
-      this.intTestArray = list;  
-   }
-   
+{   
    // method
-   public void printIntArray()
+   public static void printIntArray(int[] list)
    {   
-      if(this.intTestArray.length == 0)
+      if(list.length == 0)
       {
          System.out.print("[]");
       }
       
       else
       {
-         System.out.print("[" + this.intTestArray[0]);
+         System.out.print("[" + list[0]);
          
-         for(int i=1; i < this.intTestArray.length; i++)
+         for(int i=1; i < list.length; i++)
          {
-            System.out.print(", " + this.intTestArray[i]);
+            System.out.print(", " + list[i]);
          }
          
          System.out.println("]");
       }
   }
   
-  public int count(int number)
-  {
-      // loop through the array
-      // if array[i] == number, count++
-      // return count
-      
+  public static int count(int number, int[] list)
+  {     
       int count = 0;
       
-      for(int i=0; i < this.intTestArray.length; i++)
+      for(int i=0; i < list.length; i++)
       {
-         if(intTestArray[i] == number)
+         if(list[i] == number)
          {
             count++;
          }
@@ -48,6 +35,71 @@ public class IntArray
       
       return count;  
   }
+  
+  public static int indexOf(int target, int[] list)
+  {      
+      for(int i=0; i < list.length; i++)
+      {
+         if(list[i] == target)
+         {
+            return i;
+         }   
+      }
+      
+      return -1;
+  }
+  
+  public static void replaceAll(int target, int replacement, int[] list)
+  {
+      for(int i=0; i < list.length; i++)
+      {
+         if(list[i] == target)
+         {
+            list[i] = replacement;
+         }
+      }        
+  }
+  
+  public static boolean equals(int[] list1, int[] list2)
+  {
+      
+      if(list1.length != list2.length)
+      {
+         return false;
+      }
+      
+      for(int i = 0; i < list1.length; i++)
+      {
+         if(list1[i] != list2[i])
+         {
+            return false;   
+         }
+      }  
+      
+      return true;     
+  }
+  
+  public static void reverse(int[] list)
+  {
+      
+      int lastIndex = list.length - 1;
+      
+      for(int i = 0; i < (list.length / 2); i++)
+      {
+         swap(list, i , lastIndex);
+         lastIndex--;   
+      } 
+  }
+    
+  public static void swap(int[] list, int i, int j)
+  {
+      int temp = list[i];
+      list[i] = list[j];
+      list[j] = temp; 
+  }
+  
+  
+  
   
   
    
--- Arrays/IntArray/IntArrayMain.java
+++ Arrays/IntArray/IntArrayMain.java
@@ -2,11 +2,28 @@ public class IntArrayMain
 {
    public static void main(String[] args)
    {
-      int[] myTestArray = {2, 4, 5, 2, 9};
+      int[] myArray1 = {2, 4, 5, 2, 9};
+      int[] myArray2 = {23, 57, 90, 12, 3};
+      int[] myArray3 = {2, 4, 5, 2, 9};
       
-      IntArray myTestIntArray = new IntArray(myTestArray);
+      //IntArray.printIntArray(myArray1);
+      //System.out.println(myTestIntArray.count(9));
       
-      myTestIntArray.printIntArray();
-      System.out.println(myTestIntArray.count(9));
+      //System.out.println(myTestIntArray.indexOf(11));
+      
+      //myTestIntArray.replaceAll(2, 7);
+      
+      //myTestIntArray.printIntArray();
+      
+      //System.out.println(IntArray.equals(myArray1, myArray3));
+
+      IntArray.printIntArray(myArray2);
+      //IntArray.swap(myArray1, 0, 1);
+      
+      IntArray.reverse(myArray2);
+
+      IntArray.printIntArray(myArray2);
+      
+      
    }
 }
