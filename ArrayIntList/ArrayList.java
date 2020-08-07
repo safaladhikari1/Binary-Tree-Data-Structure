e4d28d4a2eae66f1b9a6f0f86048eb1b921cc164
Safal Adhikari <safal.adhikari1@outlook.com>
2020-08-06 18:12:00 -0700

    adds picture of JavaCollectionFramework as reference

--- ArrayIntList/ArrayList.java
+++ ArrayIntList/ArrayList.java
@@ -58,7 +58,7 @@ public class ArrayList<E>
    public static final int DEFAULT_CAPACITY = 100;
    
    //fields
-   private E[] elementData;    // list of integers
+   private E[] elementData;    // list of data
    private int size;             // number of elements in the list
    
    //constructor
