49841c4b5dcbacd137cac4c10ac61030d5920701
Safal Adhikari <safal.adhikari1@outlook.com>
2020-08-19 11:12:19 -0700

    adds code on how to use filter, distinct on Streams

--- FunctionalProgramming/SumStreams.java
+++ FunctionalProgramming/SumStreams.java
@@ -43,7 +43,7 @@ public class SumStreams
          Read this on a high-level:
          To assign the variable sum, first form the given range of integers, then map the
          given function over those integers, and then find their sum.
-      */
+      
       
       int sum2 = IntStream.range(1, 6) // source sequence [1, 2, 3, 4, 5]
          .map(n -> n * n) // modifier [1, 4, 9, 16, 25]
@@ -51,8 +51,49 @@ public class SumStreams
       
       System.out.println(sum2);
       
+      */
       
+      // Using Filter
+      /*
+      int sum = IntStream.of(3, 1, 4, 1, 5, 9, 2, 6, 5, 3)
+         .map(n -> n * n)
+         .sum();
+      */
+      
+      // Filter method takes as its argument a Boolean-valued predicate.
+      // we will test that the remainder when divided by 2 is not 0 (if the number is odd)
+      
+      /*
+      int sum = IntStream.of(3, 1, 4, 1, 5, 9, 2, 6, 5, 3)
+         .filter(n -> n%2 != 0) //[3, 1, 1, 5, 9, 5, 3]
+         .map(n -> n * n) //[9, 1, 1, 25, 81, 25, 9]
+         .sum(); //[151]
+      
+      System.out.println(sum);
+      */
+      
+      // Distinct removes the duplicates
+      /*
+      int sum = IntStream.of(3, 1, 4, 1, 5, 9, 2, 6, 5, 3)
+         .filter(n -> n%2 != 0) //[3, 1, 1, 5, 9, 5, 3]
+         .distinct() //[3, 1, 5, 9]
+         .map(n -> n*n) //[9, 1, 25, 81]
+         .sum(); // [116]
+      
+      System.out.println(sum);
+      */
+      
+      // Using Reduce, we want to take all of the integers in a stream
+      // and combine them into one integer using multiplication.
+      
+      System.out.println(factorial(5));
       
          
    }
+   
+   public static int factorial(int n)
+   {
+      return IntStream.range(2, n+1)
+      .reduce(1, (a, b) -> a*b);
+   } 
 }  
