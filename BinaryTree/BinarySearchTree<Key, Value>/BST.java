f7a1b189506a772dda3a14dfe7ea3450b1cbce4a
Safal Adhikari <safal.adhikari1@outlook.com>
2021-01-31 19:29:09 -0800

    adds BST<key, value> implementation, get() and put()

---
+++ BinaryTree/BinarySearchTree<Key, Value>/BST.java
@@ -0,0 +1,120 @@
+/*
+   Implementation of Binary Search Tree data structure using key, value pairs (ordered symbol-table)
+   
+   Key extends Comparable, this means:
+      Key can only be that class, which has implemented Comparable<Key> interface, with the implementation of compareTo() method.
+
+*/
+
+class BST<Key extends Comparable<Key>, Value>
+{
+   private Node root;
+   
+   private class Node
+   {
+      // key
+      private Key key;  
+      
+      // associated value
+      private Value val;
+      
+      // links to subtrees
+      private Node left, right;   
+      
+      // no. of nodes in subtree
+      private int n;
+      
+      public Node(Key key, Value val, int n)
+      {
+         this.key = key;
+         this.val = val;
+         this.n = n;
+      }  
+   }    
+      
+   public int size()
+   {
+      return size(root);
+   }
+   
+   private int size(Node x)
+   {
+      if(x == null)
+      {
+         return 0;
+      }
+      else
+      {
+         return x.n;
+      }
+   }
+   
+   // Review pictures on Page 400
+   public Value get(Key key)
+   {
+      return get(root, key);
+   }
+   
+   private Value get(Node x, Key key)
+   {
+      // Return value associated with key in the subtree rooted at x
+      // return null if key is not present in subtree rooted at x
+      
+      if(x == null)
+      {
+         return null;
+      }
+      
+      int cmp = key.compareTo(x.key);
+      
+      if(cmp < 0)
+      {
+         return get(x.left, key);
+      }
+      else if(cmp > 0)
+      {
+         return get(x.right, key);
+      }
+      else
+      {
+         return x.val;
+      }
+   }
+   
+   public void put(Key key, Value val)
+   {
+      // Search for key. Update value if found; grow table if new.
+      
+      root = put(root, key, val);
+   }
+   
+   private Node put(Node x, Key key, Value val)
+   {
+      // Change key's value to val if key in subtree rooted at x.
+      // Otherwise, add new node to subtree associating key with val.
+      
+      if(x == null)
+      {
+         return new Node(key, val, 1);
+      }
+      
+      int cmp = key.compareTo(x.key);
+      
+      if(cmp < 0)
+      {
+         x.left = put(x.left, key, val);
+      }
+      else if(cmp > 0)
+      {
+         x.right = put(x.right, key, val);
+      }
+      else
+      {
+         x.val = val;
+      }
+      
+      x.n = size(x.left) + size(x.right) + 1;
+      
+      return x;
+   }
+}
